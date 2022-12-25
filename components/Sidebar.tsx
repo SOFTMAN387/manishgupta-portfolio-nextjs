2c935691cc3b6e75e9d3075449547e7879f0f7d2
Manish Kumar Gupta <manishkumarpatna2020@gmail.com>
2022-12-25 12:44:32 +0530

    changed resume

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
       </p>
       {/* Resume */}
       <a
-        href="/resume/manishgupta.pdf"
+        href="/resume/mkgresume.pdf"
         download="Manish Gupta Resume.pdf"
         className="flex items-center justify-center px-2 py-1 my-2 bg-gray-200 rounded-full cursor-pointer dark:bg-dark-200 dark:bg-black-500"
       >
